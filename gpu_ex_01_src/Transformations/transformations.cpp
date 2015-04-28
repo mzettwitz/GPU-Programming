f24fb8ceabd5f8c166a0bee33fbc8138359bfa10
Martin Zettwitz <00zetti@gmail.com>
2015-04-29 00:08:41 +0200

    Added useful(!) comments, Added Rotation to cone, TODO: line from cone to origin

--- gpu_ex_01_src/Transformations/transformations.cpp
+++ gpu_ex_01_src/Transformations/transformations.cpp
@@ -1,8 +1,7 @@
-// *** Transformationen
+﻿// *** Transformationen
 
 #include <math.h>
 #include <GL/freeglut.h>
-#include <stdio.h>
 
 #define PI 3.141592f
 
@@ -12,15 +11,13 @@
 int width = 600;
 int height = 600;
 
-bool to = true;
-
 float theta = PI / 2.0f - 0.4f;
 float phi = 0.0f;
 float distance = 25.0f;
 float oldX, oldY;
 int motionState;
 
-// Winkel, der sich kontinuierlich erh�ht. (Kann f�r die Bewegungen auf den Kreisbahnen genutzt werden)
+// Winkel, der sich kontinuierlich erhöht. (Kann für die Bewegungen auf den Kreisbahnen genutzt werden)
 float angle = 0.0f;
 float speed = 1.f;
 
@@ -35,7 +32,7 @@ void drawCircle(float radius, int resolution)
 
 	glBegin(GL_LINE_STRIP);
 	for (int i = 0; i <= resolution; ++i)
-	{ 
+	{
 		float alpha = float(i * (360.f / float(resolution)));
 		glVertex3f(radius *cos(toRad(alpha)), 0, radius *sin(toRad(alpha)));
 		glColor3f(1.f, 0.f, 0.f);
@@ -46,6 +43,17 @@ void drawCircle(float radius, int resolution)
 	glEnable(GL_LIGHTING);
 }
 
+/*
+void drawLine(glVertex3f start, glVertex3f end)
+{
+	glBegin(GL_LINE);
+	start;
+	glColor3f(1.0f, 0.0f, 0.0f, 1.0f);
+	end;
+	glColor3f(1.0f, 0.0f, 0.0f, 1.0f);
+	glEnd();
+}
+*/
 
 void display(void)
 {
@@ -60,55 +68,41 @@ void display(void)
 	float z = distance * sin(theta) * sin(phi);
 	gluLookAt(x, y, z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-	float model[16];
-	float pos[4] = { 0, 0, 0, 1 };
-	float newPos[4];
-
-	glPushMatrix();
 	// Teekanne rendern.
 	glutSolidTeapot(1);
-	//teapot matrix
 
-	//outer rim
+	// Den Matrix-Stack sichern.	
+	glPushMatrix();
+
+	// Zeichnen der Kugelkreisbahn.
 	drawCircle(10.f, 100);
-	//sphere
+
+	// Zeichnen der Kugel.
+	// Wenden Sie eine Translation und eine Rotation an, bevor sie die Kugel zeichnen. Sie k�nnen die Variable 'angle' für die Rotation verwenden.
+	// Bedenken Sie dabei die richtige Reihenfolge der beiden Transformationen.
 	glRotatef(angle * speed, 0, 1, 0);
 	glTranslatef(10.f, 0.f, 0.f);
-	glPushMatrix();
 	glutSolidSphere(1, 25, 25);
 
-
-	glGetFloatv(GL_MODELVIEW_MATRIX, model);
-	newPos[0] = pos[0] * model[0] + pos[1] * model[4] + pos[2] * model[8] + pos[3] * model[12];
-	newPos[1] = pos[0] * model[1] + pos[1] * model[5] + pos[2] * model[9] + pos[3] * model[13];
-	newPos[2] = pos[0] * model[2] + pos[1] * model[6] + pos[2] * model[10] + pos[3] * model[14];
-	newPos[3] = pos[0] * model[3] + pos[1] * model[7] + pos[2] * model[11] + pos[3] * model[15];
-
-	glPopMatrix();
-
-	glDisable(GL_LIGHTING);
-	glBegin(GL_LINES);
-	glVertex4f(0, 0, 0, 1);
-	glColor3f(1.f, 0.f, 0.f);
-	glVertex4f(newPos[0], newPos[1], newPos[2], newPos[3]);
-	glColor3f(1.f, 0.f, 0.f);
-	glEnd();
-	glEnable(GL_LIGHTING);
+	// Zeichnen der Würfelkreisbahn.
+	// Hinweis: Der Ursprung des Koordinatensystems befindet sich nun im Zentrum des Würfels.
+	// Drehen Sie das Koordinatensystem um 90° entlang der Achse, die für die Verschiebung des Würfels genutzt wurde.
+	// Danach steht die Würfelkreisbahn senkrecht zur Tangentialrichtung der Kugelkreisbahn.
 	glPushMatrix();
-	glLoadMatrixf(model);
 
-	//circle
+
 	glRotatef(90.f, 1.f, 0.f, 0.f);
 	drawCircle(5.f, 100);
-	//cube
+
+	// Zeichnen des Würfels.
+	// Wenden Sie die entsprechende Translation und Rotation an, bevor sie den Würfel zeichnen.
 	glRotatef(angle*speed, 0, 1, 0);
 	glTranslatef(5.f, 0.f, 0.f);
 
 	glutSolidCube(1);
 
 
-
-	//Draw line from cube to cone
+	// Zeichnen einer Linie von Würfel zu Kegel.
 	glDisable(GL_LIGHTING);
 	glBegin(GL_LINES);
 	glVertex3f(0.f, 0.f, 0.f);
@@ -118,56 +112,76 @@ void display(void)
 	glEnd();
 	glEnable(GL_LIGHTING);
 
-	glTranslatef(3.f, 0.f, 0.f);
-
-	//line from cone to 0,0,0
-	//cone position ??
-	glGetFloatv(GL_MODELVIEW_MATRIX, model);
-
-	newPos[0] = pos[0] * model[0] + pos[1] * model[4] + pos[2] * model[8] + pos[3] * model[12];
-	newPos[1] = pos[0] * model[1] + pos[1] * model[5] + pos[2] * model[9] + pos[3] * model[13];
-	newPos[2] = pos[0] * model[2] + pos[1] * model[6] + pos[2] * model[10] + pos[3] * model[14];
-	newPos[3] = pos[0] * model[3] + pos[1] * model[7] + pos[2] * model[11] + pos[3] * model[15];
-
-	//restore original matrix
-	glPopMatrix();
+	// Zeichnen des Kegels.
 	
-	glDisable(GL_LIGHTING);
-	glBegin(GL_LINES);
-	glVertex4f(0, 0, 0, 1);
-	glColor3f(1.f, 0.f, 0.f);
-	glVertex4f(newPos[0],newPos[1],newPos[2],newPos[3]);
-	glColor3f(1.f, 0.f, 0.f);
-	glEnd();
-	glEnable(GL_LIGHTING);
-	glPushMatrix();
-	glLoadMatrixf(model);
+
+	// Drehung anwenden, sodass Koordinatensystem in Richtung Ursprung orientiert ist. (Hinweis: Implementieren Sie dies zuletzt.)
+	glTranslatef(3.f, 0.f, 0.f);
+	glRotatef(90 + angle*speed, 0, -1, 0);
+
+	GLfloat length = 10 + 8 * cosf(angle*speed*PI / 180);
+	GLfloat height = 8 * sinf(angle*speed*PI / 180);
+	GLfloat heightangle = 180 / PI*atan(height / length);
+	glRotatef(heightangle, 0, 1, 0);
 
 
 	glutSolidCone(0.5, 1, 25, 25);
+
+
+	// TODO: Zeichnen der Linie von Kegel zu Urpsrung.
+	/*glBegin(GL_LINE_STRIP);
+	glVertex3f(0, 0, 0);
+	glVertex3f(cone_cent_dis * cos(cone_cent_angle), cone_cent_dis * sin(cone_cent_angle), 0);
+	glEnd();
+	*/
 	
-	glPopMatrix();	
+
+	/* MICHI
+	//Draw line 
+	GLfloat modelview[16];
+	glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
+	glVertex3f startLine = modelview * glVertex3f(0, 0, 0);
+	glTranslatef(3.f, 0.f, 0.f);
+	glVertex3f endLine = modelview * glVertex3f(0, 0, 0);
+	drawLine(start, end);
+	glTranslatef(3.f, 0.f, 0.f);
 	
-	glutSwapBuffers();	
+	
+	glPopMatrix();
+	glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
+	glPushMatrix();
+	//should be 0,0,0 anyway
+	glVertex3f coneUp = startLine;
+	startLine = modelview * glVertex3f(0, 0, 0);
+	drawLine(start, end);
+
+	
+	gluLookat(endLine.x, endLine.y, endLine.z, 0, 0, 0, endLine.x - coneUp.x, endLine.y - coneUp.y, endLine.z - coneUp.z);
+	glutSolidCone(1, 2, 25, 25);
+	
+	glPopMatrix();
+	*/
+
+	glutSwapBuffers();
 
 	angle += 1.0f / 60.0f;
-	//speed += 1.0f / 60.0f;
+	speed += 1.0f / 60.0f;
 }
 
 void mouseMotion(int x, int y)
 {
 	float deltaX = x - oldX;
 	float deltaY = y - oldY;
-	
+
 	if (motionState == ROTATE) {
 		theta -= 0.01f * deltaY;
 
 		if (theta < 0.01f) theta = 0.01f;
-		else if (theta > PI/2.0f - 0.01f) theta = PI/2.0f - 0.01f;
+		else if (theta > PI / 2.0f - 0.01f) theta = PI / 2.0f - 0.01f;
 
-		phi += 0.01f * deltaX;	
-		if (phi < 0) phi += 2*PI;
-		else if (phi > 2*PI) phi -= 2*PI;
+		phi += 0.01f * deltaX;
+		if (phi < 0) phi += 2 * PI;
+		else if (phi > 2 * PI) phi -= 2 * PI;
 	}
 	else if (motionState == MOVE) {
 		distance += 0.01f * deltaY;
@@ -217,14 +231,14 @@ int main(int argc, char **argv)
 
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
-	
+
 	glEnable(GL_DEPTH_TEST);
 
-	glViewport(0,0,width,height);					
-	glMatrixMode(GL_PROJECTION);					
-	glLoadIdentity();								
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 
-	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
