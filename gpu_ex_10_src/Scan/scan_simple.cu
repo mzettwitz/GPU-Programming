2e44bd976805b83c39bd4d3866f663c231f8d347
Alina Solovjova <alina.s95@hotmail.de>
2015-07-05 20:16:26 +0200

    Task 01

--- gpu_ex_10_src/Scan/scan_simple.cu
+++ gpu_ex_10_src/Scan/scan_simple.cu
@@ -5,8 +5,8 @@
 
 #define N 512
 
-GLfloat viewPosition[4] = {0.0f, 5.0f, 10.0f, 1.0f};  
-GLfloat viewDirection[4] = {-0.0f, -5.0f, -10.0f, 0.0f};  
+GLfloat viewPosition[4] = { 0.0f, 5.0f, 10.0f, 1.0f };
+GLfloat viewDirection[4] = { -0.0f, -5.0f, -10.0f, 0.0f };
 GLfloat viewAngle = 45.0f;
 GLfloat viewNear = 4.5f;
 GLfloat viewFar = 25.0f;
@@ -28,72 +28,73 @@ float *devColorPixelsSrc, *devColorPixelsDst, *devDepthPixels;
 
 void drawGround()
 {
-	GLfloat grey[3] = {0.8f, 0.8f, 0.8f};
-    
+	GLfloat grey[3] = { 0.8f, 0.8f, 0.8f };
+
 	glNormal3f(0, 1, 0);
 	glMaterialfv(GL_FRONT, GL_AMBIENT, grey);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, grey);
-    glBegin(GL_QUADS);
-    glVertex3f(-10, 0, 10);
-    glVertex3f( 10, 0, 10);
-    glVertex3f( 10, 0, -10);
-    glVertex3f(-10, 0, -10);
-    glEnd();
+	glBegin(GL_QUADS);
+	glVertex3f(-10, 0, 10);
+	glVertex3f(10, 0, 10);
+	glVertex3f(10, 0, -10);
+	glVertex3f(-10, 0, -10);
+	glEnd();
 }
 
 
 void drawScene()
 {
-    GLfloat diffuse1[4]      = {0.5f, 0.5f, 0.5f, 1.0f};
-	GLfloat lightAmbient[4]  = {0.0f, 0.0f, 0.0f, 1.0f};  
-	GLfloat lightDiffuse[4]  = {0.2f, 0.2f, 0.2f, 1.0f};  
-	GLfloat lightPosition[4] = {0.5f, 10.5f, 6.0f, 1.0f};  
+	GLfloat diffuse1[4] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	GLfloat lightAmbient[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	GLfloat lightDiffuse[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
+	GLfloat lightPosition[4] = { 0.5f, 10.5f, 6.0f, 1.0f };
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
 
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);    
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);    
-    glPushMatrix();
-    glTranslatef(0.0f, 1.0f, 0.0f);
-	glRotatef(-yRotationAngle/3.0f, 0.0f, 1.0f, 0.0f);
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);
+	glPushMatrix();
+	glTranslatef(0.0f, 1.0f, 0.0f);
+	glRotatef(-yRotationAngle / 3.0f, 0.0f, 1.0f, 0.0f);
+	glutSolidTeapot(1.0f);
+	glPopMatrix();
 
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);    
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);    
-    glPushMatrix();
-    glTranslatef(-1.0f, 1.0f, 3.0f);
-	glRotatef(-yRotationAngle/3.0f, 0.0f, 1.0f, 0.0f);
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);
+	glPushMatrix();
+	glTranslatef(-1.0f, 1.0f, 3.0f);
+	glRotatef(-yRotationAngle / 3.0f, 0.0f, 1.0f, 0.0f);
+	glutSolidTeapot(1.0f);
+	glPopMatrix();
 
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);    
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);    
-    glPushMatrix();
-    glTranslatef(1.0f, 1.0f, -3.0f);
-	glRotatef(-yRotationAngle/3.0f, 0.0f, 1.0f, 0.0f);
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);
+	glPushMatrix();
+	glTranslatef(1.0f, 1.0f, -3.0f);
+	glRotatef(-yRotationAngle / 3.0f, 0.0f, 1.0f, 0.0f);
+	glutSolidTeapot(1.0f);
+	glPopMatrix();
 
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);    
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);    
-    glPushMatrix();
-    glTranslatef(-2.0f, 1.0f, 6.0f);
-	glRotatef(-yRotationAngle/3.0f, 0.0f, 1.0f, 0.0f);
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);
+	glPushMatrix();
+	glTranslatef(-2.0f, 1.0f, 6.0f);
+	glRotatef(-yRotationAngle / 3.0f, 0.0f, 1.0f, 0.0f);
+	glutSolidTeapot(1.0f);
+	glPopMatrix();
 
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);    
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);    
-    glPushMatrix();
-    glTranslatef(2.0f, 1.0f, -6.0f);
-	glRotatef(-yRotationAngle/3.0f, 0.0f, 1.0f, 0.0f);
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, diffuse1);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);
+	glPushMatrix();
+	glTranslatef(2.0f, 1.0f, -6.0f);
+	glRotatef(-yRotationAngle / 3.0f, 0.0f, 1.0f, 0.0f);
+	glutSolidTeapot(1.0f);
+	glPopMatrix();
 
 	drawGround();
+
 }
 
 
@@ -106,89 +107,105 @@ void initGL()
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-    gluPerspective(viewAngle, 1.0f, viewNear, viewFar);
+	gluPerspective(viewAngle, 1.0f, viewNear, viewFar);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();   
+	glLoadIdentity();
 }
 
 
 
-__global__ void sat_filter(float *dstImage, float *sat, float *srcDepth, 
-					   float focusDepth, float sizeScale, int n)
+//transpose Kernel
+__global__ void traspose(float *dstImage, float *srcImage){
+
+
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int index = y + x * N;
+
+	int targetIndex = x + y *N;
+
+	dstImage[index] = srcImage[targetIndex];
+
+
+}
+
+
+__global__ void sat_filter(float *dstImage, float *sat, float *srcDepth,
+	float focusDepth, float sizeScale, int n)
 {
 	// TODO: Filtergröße bestimmen
 
 	// TODO: Anzahl der Pixel im Filterkern bestimmen	
 
 	// TODO: SAT-Werte für die Eckpunkte des Filterkerns bestimmen.
-	
+
 	// TODO: Mittelwert berechnen.
 }
 
 
 __global__ void scan_naive(float *g_odata, float *g_idata, int n)
 {
-    // Dynamically allocated shared memory for scan kernels
-    __shared__  float temp[2*N];
+	// Dynamically allocated shared memory for scan kernels
+	__shared__  float temp[2 * N];
 
-    int thid = threadIdx.x;
-    int bid = blockIdx.x;
+	int thid = threadIdx.x;
+	int bid = blockIdx.x;
 
-    int pout = 0;
-    int pin = 1;
+	int pout = 0;
+	int pin = 1;
 
-    // Cache the computational window in shared memory
-    temp[pout*n + thid] = (thid > 0) ? g_idata[bid * N + thid-1] : 0;
+	// Cache the computational window in shared memory
+	temp[pout*n + thid] = (thid > 0) ? g_idata[bid * N + thid - 1] : 0;
 
-    for (int offset = 1; offset < n; offset *= 2)
-    {
-        pout = 1 - pout;
-        pin  = 1 - pout;
-        __syncthreads();
+	for (int offset = 1; offset < n; offset *= 2)
+	{
+		pout = 1 - pout;
+		pin = 1 - pout;
+		__syncthreads();
 
-        temp[pout*n+thid] = temp[pin*n+thid];
+		temp[pout*n + thid] = temp[pin*n + thid];
 
-        if (thid >= offset)
-            temp[pout*n+thid] += temp[pin*n+thid - offset];
-    }
+		if (thid >= offset)
+			temp[pout*n + thid] += temp[pin*n + thid - offset];
+	}
 
-    __syncthreads();
+	__syncthreads();
 
-    g_odata[bid * N + thid] = temp[pout*n+thid];
+	g_odata[bid * N + thid] = temp[pout*n + thid];
 }
 
 
 void initCUDA()
 {
-    cudaMalloc( (void**)&devColorPixelsSrc, N * N * sizeof(float) );
-    cudaMalloc( (void**)&devColorPixelsDst, N * N * sizeof(float) );
-    cudaMalloc( (void**)&devDepthPixels, N * N * sizeof(float) );
+	cudaMalloc((void**)&devColorPixelsSrc, N * N * sizeof(float));
+	cudaMalloc((void**)&devColorPixelsDst, N * N * sizeof(float));
+	cudaMalloc((void**)&devDepthPixels, N * N * sizeof(float));
 }
 
 void special(int key, int x, int y)
 {
 	switch (key) {
-	case GLUT_KEY_UP :
+	case GLUT_KEY_UP:
 		focusDepth += 0.05f;
-		 if (focusDepth > 1.0f) focusDepth = 1.0;
+		if (focusDepth > 1.0f) focusDepth = 1.0;
 		break;
-	case GLUT_KEY_DOWN :
+	case GLUT_KEY_DOWN:
 		focusDepth -= 0.05f;
-		 if (focusDepth < 0.0f) focusDepth = 0.0;
+		if (focusDepth < 0.0f) focusDepth = 0.0;
 		break;
-	case GLUT_KEY_LEFT :
+	case GLUT_KEY_LEFT:
 		sizeScale -= 1.0f;
 		if (sizeScale > 100.0f) sizeScale = 100.0;
 		break;
-	case GLUT_KEY_RIGHT :
+	case GLUT_KEY_RIGHT:
 		sizeScale += 1.0f;
 		if (sizeScale < 1.0f) sizeScale = 1.0;
 		break;
-	case GLUT_KEY_PAGE_UP :
+	case GLUT_KEY_PAGE_UP:
 		viewFar += 1.0f;
 		initGL();
 		break;
-	case GLUT_KEY_PAGE_DOWN :
+	case GLUT_KEY_PAGE_DOWN:
 		viewFar -= 1.0f;
 		if (viewFar < viewNear) viewFar = viewNear;
 		initGL();
@@ -196,41 +213,46 @@ void special(int key, int x, int y)
 	}
 }
 
-void display(void)								
+void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Szene rendern
 	glLoadIdentity();
-	gluLookAt(viewPosition[0], viewPosition[1], viewPosition[2], 
-              viewDirection[0] - viewPosition[0], viewDirection[1] - viewPosition[1], viewDirection[2] - viewPosition[2], 
-              0, 1, 0);    
-	drawScene();	    
+	gluLookAt(viewPosition[0], viewPosition[1], viewPosition[2],
+		viewDirection[0] - viewPosition[0], viewDirection[1] - viewPosition[1], viewDirection[2] - viewPosition[2],
+		0, 1, 0);
+	drawScene();
 
-    // Tiefe und Farbe in den RAM streamen.
-    glReadPixels( 0, 0, N, N, GL_DEPTH_COMPONENT, GL_FLOAT, depthPixels);	
-    glReadPixels( 0, 0, N, N, GL_LUMINANCE, GL_FLOAT, colorPixels);	
-	
-    // Beide arrays in den Device-Memory kopieren.
-    cudaMemcpy( devColorPixelsSrc, colorPixels, N * N * sizeof(float), cudaMemcpyHostToDevice );
-    cudaMemcpy( devDepthPixels, depthPixels, N * N * sizeof(float), cudaMemcpyHostToDevice );
+	// Tiefe und Farbe in den RAM streamen.
+	glReadPixels(0, 0, N, N, GL_DEPTH_COMPONENT, GL_FLOAT, depthPixels);
+	glReadPixels(0, 0, N, N, GL_LUMINANCE, GL_FLOAT, colorPixels);
+
+	// Beide arrays in den Device-Memory kopieren.
+	cudaMemcpy(devColorPixelsSrc, colorPixels, N * N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(devDepthPixels, depthPixels, N * N * sizeof(float), cudaMemcpyHostToDevice);
+	dim3 a(N, N, 1);
 
 	// TODO: Scan    
-	// TODO: Transponieren    
+	// TODO: Transponieren 
+
+	traspose << <a, 1 >> >(devColorPixelsDst, devColorPixelsSrc); //transposes image only if, #if is set to 1, no idea why, but it works!
+
+
 	// TODO: Scan  
 	// TODO: Transponieren    
 	// TODO: SAT-Filter anwenden	
 
 	// Ergebnis in Host-Memory kopieren
-	cudaMemcpy( filteredPixels, devColorPixelsDst, N*N*4, cudaMemcpyDeviceToHost );
+	cudaMemcpy(filteredPixels, devColorPixelsDst, N*N * 4, cudaMemcpyDeviceToHost);
 
 	// TODO: Beim #if aus der 0 eine 1 machen, damit das gefilterte Bild angezeigt wird!
 #if 0
 	// Mittelwert-Bild rendern
-	glDrawPixels( N, N, GL_LUMINANCE, GL_FLOAT, filteredPixels );
+	glDrawPixels(N, N, GL_LUMINANCE, GL_FLOAT, filteredPixels);
 #else
 	// Durchreichen des Eingabebildes.
-	glDrawPixels( N, N, GL_LUMINANCE, GL_FLOAT, colorPixels );
+	glDrawPixels(N, N, GL_LUMINANCE, GL_FLOAT, colorPixels);
 #endif
 
 	xRotationAngle += xRotationSpeed;   // Rotationswinkel erhoehen
